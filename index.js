d51101f749d768c46cd46500e35654f923d808be
Mihail Marian <mihailmarian12@gmail.com>
2021-09-23 17:50:54 +0100

    first interaction with ethereum api

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const { ethers } = require("ethers");
 
+async function showBalance(provider, address) {
+    balanceObj = await provider.getBalance(address);
+    balanceNum = ethers.utils.formatEther(balanceObj);
+    console.log(balanceNum);
+}
+
 const url = "https://rinkeby.infura.io/v3/";
 const infuraKey = process.env.INFURA_KEY;
 
@@ -8,4 +14,6 @@ const provider = new ethers.providers.JsonRpcProvider(url+infuraKey);
 
 const signer = provider.getSigner();
 
-console.log(provider,signer);
+address = "0x04d5Bc320FFa275E77BA576B608798418e83B360";
+
+showBalance(provider,address);
