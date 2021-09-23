864a5ee8277bfbef13bff13ee2240b25a38bc8d9
Mihail Marian <mihailmarian12@gmail.com>
2021-09-23 17:45:03 +0100

    used ethers

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 require('dotenv').config();
+const { ethers } = require("ethers");
 
-console.log(process.env.INFURA_KEY);
+const url = "https://rinkeby.infura.io/v3/";
+const infuraKey = process.env.INFURA_KEY;
+
+const provider = new ethers.providers.JsonRpcProvider(url+infuraKey);
+
+const signer = provider.getSigner();
+
+console.log(provider,signer);
