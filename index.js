7e6883e0fc3e8ffdacce2bb7f439c983b5958cce
Mihail Marian <mihailmarian12@gmail.com>
2021-09-24 08:48:00 +0100

    new method works on balance

--- index.js
+++ index.js
@@ -1,100 +1,33 @@
 require('dotenv').config();
-const { ethers } = require("ethers");
+const Web3 = require('web3');
+const Provider = require('@truffle/hdwallet-provider');
 
-async function showBalance(provider, address) {
-    balanceObj = await provider.getBalance(address);
-    balanceNum = ethers.utils.formatEther(balanceObj);
-    console.log("Balance of user is " + balanceNum);
-}
-
-function getContract(provider) {
-    const address = "0x7614A103007DE7A9157FAD6ab73361AF2AdeAb64";
-
-    const abi = [
-        {
-            "inputs": [],
-            "stateMutability": "nonpayable",
-            "type": "constructor"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "int256",
-                    "name": "amt",
-                    "type": "int256"
-                }
-            ],
-            "name": "deposit",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        },
-        {
-            "inputs": [],
-            "name": "getBalance",
-            "outputs": [
-                {
-                    "internalType": "int256",
-                    "name": "",
-                    "type": "int256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "int256",
-                    "name": "amt",
-                    "type": "int256"
-                }
-            ],
-            "name": "withdraw",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ]
-    
-    return new ethers.Contract(address, abi, provider)
-}
-
-async function showContractBalance(contract) {
-    balance = await contract.getBalance();
-    console.log("Contract balance is " + ethers.utils.formatEther(balance));
-}
-
-async function deposit(contract) {
-
-    const signedContract = await contract.connect(signer);
-
-    await signedContract.deposit(5000);
-
-    //showContractBalance(signedContract);
-}
-
-const url = "https://rinkeby.infura.io/v3/";
-const infuraKey = process.env.INFURA_KEY;
-
-const provider = new ethers.providers.JsonRpcProvider(url+infuraKey);
-
-//const signer = provider.getSigner();
+const InfuraUrl = "https://rinkeby.infura.io/v3/";
+const InfuraKey = process.env.INFURA_KEY;
+const PrivateKey = process.env.PRIVATE_KEY;
+const MyContract = require("./contract.json");
 
 inputAddress = "0x04d5Bc320FFa275E77BA576B608798418e83B360";
 
-showBalance(provider,inputAddress);
+//Easy way (Web3 + @truffle/hdwallet-provider)
+const init3 = async () => {
+    const provider = new Provider(PrivateKey, InfuraUrl + InfuraKey); 
+    const web3 = new Web3(provider);
+    const myContract = new web3.eth.Contract(
+      MyContract.abi,
+      MyContract.address
+    );
 
-const contract = getContract(provider);
+    const balance = await myContract.methods.getBalance().call();
 
+    console.log(balance);
 
-async function getBalance(provider,signer) {
-    console.log(signer.address);
-    balance = await provider.send('relay_getBalance', [signer.address])
-    console.log(`Your current ITX balance is ${balance}`)
+    /*
+    console.log(`Old data value: ${await myContract.methods.data().call()}`);
+    const receipt = await myContract.methods.setData(3).send({ from: address });
+    console.log(`Transaction hash: ${receipt.transactionHash}`);
+    console.log(`New data value: ${await myContract.methods.data().call()}`);
+    */
 }
-
-showContractBalance(contract);
-
-const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-getBalance(provider,signer)
+  
+init3();
